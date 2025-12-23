95f2f0d143713e9325dc8b3ddece14eb03187812
Ivriel <gunawanivriel@gmail.com>
2025-12-23 21:37:31 +0700

    Blade component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       @vite('resources/css/app.css')
-    <title>Halaman Contact</title>
-</head>
-<body>
-       <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1 class="text-2xl text-bold text-blue-500">Halaman Contact</h1>
-    <ul>
-        <li>
-            <a href="https://instagram.com/wynsc_u13">Instagram</a>
-        </li>
-        <li>
-            <a href="www.ivriel.my.id">Portfolio</a>
-        </li>
-        <li>
-           <b>Phone: </b> <p>085157344265</p>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-3xl">Halaman Contact</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        'title'=>'Home Page'
+    ]);
 });
 
 Route::get('/about',function(){
     return view('about',[
-        'nama' => 'Ivriel Gunawan'
+        'name' => 'Ivriel Gunawan',
+        'title'=> 'About'
     ]);
 });
 
 Route::get('/blog',function(){
-    return view('blog');
+    return view('blog',[
+        'title' => 'Blog'
+    ]);
 });
 
 Route::get('/contact',function(){
-    return view('contact');
+    return view('contact',[
+        'title'=> 'Contact'
+    ]);
 });

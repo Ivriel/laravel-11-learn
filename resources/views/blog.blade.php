3a3c6f8fbf0db84910316cebe017d9d66287e70a
Ivriel <gunawanivriel@gmail.com>
2025-12-23 18:06:40 +0700

    Struktur folder

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Article</title>
+</head>
+<body>
+    <h1>Lorem ipsum</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid magni necessitatibus asperiores sequi minima? Tenetur, suscipit asperiores voluptas minima laborum qui sed impedit! Voluptatibus ea, numquam praesentium libero ducimus nobis iusto consequatur. Deserunt aliquam labore expedita veritatis ullam numquam dolorem enim, libero vero excepturi cum earum, asperiores perferendis eaque necessitatibus, doloremque illo at reiciendis omnis! Aut architecto aliquid, dolor fugiat, commodi voluptatem reprehenderit nemo necessitatibus sed vel, suscipit accusantium corrupti assumenda esse velit optio eum pariatur eius officia tempora est natus. Fugiat nesciunt iure id quaerat explicabo voluptate! Libero deleniti porro earum vel aliquid ab asperiores sed tenetur? Ab, nulla!</p>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+    <ul>
+        <li><b>Email: </b> gunawanivriel@gmail.com</li>
+        <li><b>Instagram: </b>wynsc_u13</li>
+        <li><b>Phone: </b>085157344265 </li>
+    </ul>
+</body>
+</html>

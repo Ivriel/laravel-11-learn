7f68bb4c289bd53a6658e871d3c35fb1899e5f5d
Ivriel <gunawanivriel@gmail.com>
2025-12-24 20:01:10 +0700

    View Data

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,73 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home',[
-        'title'=>'Home Page'
+    return view('home', [
+        'title' => 'Home Page',
     ]);
 });
 
-Route::get('/about',function(){
-    return view('about',[
+Route::get('/about', function () {
+    return view('about', [
         'name' => 'Ivriel Gunawan',
-        'title'=> 'About'
+        'title' => 'About',
     ]);
 });
 
-Route::get('/blog',function(){
-    return view('blog',[
-        'title' => 'Blog'
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Blog',
+        'posts' => [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Ivriel Gunawan',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam doloribus beatae blanditiis ducimus vero repellendus nesciunt quo',
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Ivriel Gunawan',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam doloribus beatae blanditiis ducimus vero repellendus nesciunt quo osasmksas  osas huvuveve',
+            ],
+        ],
     ]);
 });
 
-Route::get('/contact',function(){
-    return view('contact',[
-        'title'=> 'Contact'
+Route::get('/contact', function () {
+    return view('contact', [
+        'title' => 'Contact',
+    ]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Ivriel Gunawan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam doloribus beatae blanditiis ducimus vero repellendus nesciunt quo',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Ivriel Gunawan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aperiam doloribus beatae blanditiis ducimus vero repellendus nesciunt quo osasmksas  osas huvuveve',
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post,
     ]);
 });

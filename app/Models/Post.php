3f8ee3e38ea68fa3c62d5f0c830af234fc82b698
Ivriel <gunawanivriel@gmail.com>
2025-12-28 17:12:31 +0700

    Searching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -26,5 +27,24 @@ class Post extends Model
     public function category():BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function scopeFilter(Builder $query,array $filters) 
+    {
+        $query->when($filters['search'] ?? false, function($query) use ($filters) {
+            $query->where('title','like','%' . $filters['search'] . '%');
+        });
+
+        $query->when($filters['category'] ?? false, function($query) use ($filters) {
+            $query->whereHas('category', function($query) use ($filters) {
+                $query->where('slug', $filters['category']);
+            });
+        });
+
+           $query->when($filters['author'] ?? false, function($query) use ($filters) {
+            $query->whereHas('author', function($query) use ($filters) {
+                $query->where('username', $filters['author']);
+            });
+        });
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -30,9 +30,9 @@ URL: https://flowbite.com/docs/components/typography/
                   <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                       <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="{{ $post->author->name }}">
                       <div>
-                          <a href="/authors/{{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
+                          <a href="/posts?author={{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
                           <p class="text-base text-gray-500 dark:text-gray-400">{{ $post->created_at->diffForHumans() }}</p>
-                            <a href="/categories/{{ $post->category->slug }}">
+                            <a href="/posts?category={{ $post->category->slug }}">
                             <span
                                 class="
                                 @if($post->category->color == 'red') bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-800
